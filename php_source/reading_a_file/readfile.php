3d6b0cee2ccd72b00448969c6cc831ae536693a9
moses <academicsnotes@gmail.com>
2021-11-11 14:35:15 +0800

    Commit codes

---
+++ php_source/reading_a_file/readfile.php
@@ -0,0 +1,8 @@
+<?php
+
+//readfile('php_source\reading_a_file\YesterdayOnceMore.txt');
+//echo file_get_contents('php_source\reading_a_file\YesterdayOnceMore.txt');
+
+$lines = file('php_source\reading_a_file\YesterdayOnceMore.txt');
+//var_dump($lines);
+?>
