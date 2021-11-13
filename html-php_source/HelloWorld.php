1f3a4f0f9fe48c685802c03be0f44e52e54e5530
moses <academicsnotes@gmail.com>
2021-11-13 14:08:50 +0800

    Commit code

---
+++ html-php_source/HelloWorld.php
@@ -0,0 +1,16 @@
+<html lang="en">
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <?php
+            // Single-line comments are like this
+            /*
+               block comments
+            */
+        ?>
+        <?php echo "Hello World"; ?><br />
+        <?php echo "Hello" . "World!"; ?><br />
+        <?php echo 2 + 3; ?>
+    </body>
+</html>
