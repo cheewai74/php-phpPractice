8797ea487614823158e34e8429f9329f7bec5730
moses <academicsnotes@gmail.com>
2021-11-26 22:23:29 +0800

    Commit script

--- OOP/Class/Example1/class_methods.php
+++ OOP/Class/Example1/class_methods.php
@@ -8,6 +8,12 @@ class Student {
     function say_hello(){
         return "Hello World!";
     }
+
+    function full_name(){
+        return $this->first_name . " " . $this->last_name;
+
+    }
+
 }
 
 $student1 = new Student;
@@ -21,6 +27,7 @@ $student2->last_name = 'Mertz';
 echo $student1->first_name . " " . $student1->last_name ."<br />";
 echo $student2->first_name . " " . $student2->last_name ."<br />";
 
+echo $student1->full_name() . "<br />";
 echo $student1->say_hello()  . "<br />";
 echo $student2->say_hello()  . "<br />";
 
