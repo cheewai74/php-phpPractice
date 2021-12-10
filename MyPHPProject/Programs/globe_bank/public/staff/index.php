45421e5d1ee0a0ca1766c33af05fb684de45a58a
moses <academicsnotes@gmail.com>
2021-12-10 15:58:16 +0800

    Commit script

--- MyPHPProject/Programs/globe_bank/public/staff/index.php
+++ MyPHPProject/Programs/globe_bank/public/staff/index.php
@@ -1,24 +1,4 @@
-<!doctype html>
-
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
-  </head>
-
-  <body>
-      <header>
-          <h1>GBI Staff Area</h1>
-      </header>
-      <navigation>
-          <ul>
-              <li><a href="index.php">Menu</a></li>
-          </ul>
-      </navigation>
-      <div id="content"></div>
-      <footer>
-          &copy; <?php echo date('Y'); ?> Globe Bank
-      </footer>
-  </body>
-</html>
+<?php require_once('../../private/initialize.php'); ?>
+<?php include('../../private/shared/staff_header.php'); ?>
+<div id="content"></div>
+<?php include('../../private/shared/staff_footer.php'); ?>
