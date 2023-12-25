ec059c089f61de64a8500fc46e194ea2124b936b
cheewai wong <ytransform_academics@outlook.com>
2023-12-25 11:29:07 +0800

    Modified

--- php_source/Basic/Array/array_deref.php
+++ php_source/Basic/Array/array_deref.php
@@ -1,8 +1,9 @@
 <?php 
-$info = getimagesize('php_source\Basic\Array\pandan.jpg');
+
+$info = getimagesize('C:\Users\dell\OneDrive\Documents\Git\php-phpPractice\php_source\Basic\Array\pandan.jpg');
 print_r($info);
 
-$mime = getimagesize('php_source\Basic\Array\pandan.jpg')['mime'];
+$mime = getimagesize('C:\Users\dell\OneDrive\Documents\Git\php-phpPractice\php_source\Basic\Array\pandan.jpg')['mime'];
 echo $mime;
 
 ?>
